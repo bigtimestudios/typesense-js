b6106e2d3c5f3d06df894f6d69830125598984b7
hortelanobruno-ol <bruno@openloot.com>
2024-05-30 08:37:05 -0300

    fix Documents.import JSON.parse error

--- src/Typesense/Documents.ts
+++ src/Typesense/Documents.ts
@@ -220,7 +220,7 @@ export default class Documents<T extends DocumentSchema = {}>
     })
 
     if (Array.isArray(documents)) {
-      const resultsInJSONFormat = resultsInJSONLFormat.split('\n').map((r) => JSON.parse(r)) as ImportResponse[]
+      const resultsInJSONFormat = resultsInJSONLFormat.split('\n').filter(Boolean).map((r) => JSON.parse(r)) as ImportResponse[]
       const failedItems = resultsInJSONFormat.filter((r) => r.success === false)
       if (failedItems.length > 0) {
         throw new ImportError(
